7044ab4c5d31b5db37c5fd7992c2752799acbe3f
Mai Quang Tuyen <maituyen100897@gmail.com>
2019-03-25 23:06:51 +0700

    init app

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var bodyparser = require('body-parser');
+var app = express();
+var server = require('http').createServer(app);
+
+var router = require('./routes/router')
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.use('/', router);
+
+server.listen(8000);
---
+++ routes/router.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var userName = 'maituyen';
+var passWord = '123456';
+
+router.get('/login', function(request, response) {
+    response.render('login');
+})
+
+router.post('/login', function(request, response) {
+    var body = request.body;
+    if (userName == body.username && passWord == body.password) {
+        response.render('./demo');
+    }
+    else {
+        response.redirect('/login');
+    }
+})
+
+module.exports = router;
